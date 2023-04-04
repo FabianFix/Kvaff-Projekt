728c4fb6f9a2e9089f0d3b62b9379acaf9ddfb0d
Fabian Forsberg <carlfabianforsberg@gmail.com>
2023-04-04 19:13:15 +0200

    data set from scb imported

---
+++ kommun.py
@@ -0,0 +1,10 @@
+class Kommun: 
+    def __init__(self, namn) -> None:
+        self.data = {
+            "månad": {
+            "ålder1": int,
+            "bla bla bla": int,
+            "total": int
+            }
+        }
+        pass
