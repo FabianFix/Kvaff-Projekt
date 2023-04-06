e7c97b5ab9157b9089cd5bbc4e6026e3bcf8766e
FabianFix <52410356+FabianFix@users.noreply.github.com>
2023-04-06 11:24:23 +0200

    ändrade månad i bankomat

--- bankomat.py
+++ bankomat.py
@@ -13,15 +13,30 @@ class Bankomat:
 
         self.ärUte = ärUte
         self.transaktionsDataSEK = []
-        self.transaktionsDataAnnan = []
+        self.transaktionsDataEUR = []
+        self.transaktionsDataUSD = []
 
     def __str__(self) -> str:
-        return f"Bankomat med id {self.id}, som finns på addressen {self.geographicalData['address']}, i {self.geographicalData['kommun']} kommun. Det finns {len(self.transaktionsDataSEK) + len(self.transaktionsDataAnnan)} transaktionsadata registrerade. ÄrUte = {self.ärUte}"
+        return f"Bankomat med id {self.id}, som finns på addressen {self.geographicalData['address']}, i {self.geographicalData['kommun']} kommun. Det finns {len(self.transaktionsDataSEK) + len(self.transaktionsDataUSD) + len(self.transaktionsDataEUR)} transaktionsadata registrerade. ÄrUte = {self.ärUte}"
     
     def läggTillTransaktion(self, månad:str, antalTransaktioner:int, valuta:str, omsättning:int): 
         if valuta == "SEK":
             self.transaktionsDataSEK.append({
-                "månad": månad,
+                "månad": månad[0:4] + "M" + månad[4:],
+                "antalTransaktioner": antalTransaktioner,
+                "omsättning": omsättning
+            })
+        
+        if valuta == "USD":
+            self.transaktionsDataUSD.append({
+                "månad": månad[0:4] + "M" + månad[4:],
+                "antalTransaktioner": antalTransaktioner,
+                "omsättning": omsättning
+            })
+        
+        if valuta == "EUR":
+            self.transaktionsDataEUR.append({
+                "månad": månad[0:4] + "M" + månad[4:],
                 "antalTransaktioner": antalTransaktioner,
                 "omsättning": omsättning
             })
